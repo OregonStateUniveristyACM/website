efb3ca7deabc5d08238509571fded1ed1cbe5818
Connor Christensen <chriconn@oregonstate.edu>
2017-08-22 23:10:38 -0700

    started building a gulp file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # website
 This is the github for Oregon State University ACM's new website
+
+
+# Development
+
+### Gulp Build
+https://fettblog.eu/gulp-4-parallel-and-series/
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,38 @@
 var gulp = require('gulp');
+var plugin = require('gulp-load-plugins')();
+
+var root = "site/";
+var destination = "dest/";
+var paths = {
+    scss: root + "**/*.scss",
+    js:   root + "**/.js",
+    html: root + "**/.html"
+};
+
+// Compile Our Sass
+gulp.task('scss', function() {
+    return gulp.src(paths.scss)
+        .pipe(plugin.sass())
+        .pipe(gulp.dest(destination));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src(paths.js)
+        .pipe(gulp.dest(destination));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch(paths.js, ['scripts']);
+    gulp.watch(paths.scss, ['scss']);
+});
+
+//remove .DS_Store files
+gulp.task( 'clean:ds', function() {
+  return gulp.src(root+"/**/.DS_Store", { read: false })
+    .pipe( rm() );
+})
+
+// Default Task
+gulp.task('default', gulp.series('scss', 'scripts', 'watch'));
