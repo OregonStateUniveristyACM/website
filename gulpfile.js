b21a136a4dfc3ba29a3f5faeb84870262ac961f9
Connor Christensen <chriconn@oregonstate.edu>
2017-08-22 23:19:10 -0700

    added clean ability to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -24,15 +24,22 @@ gulp.task('scripts', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch(paths.js, ['scripts']);
-    gulp.watch(paths.scss, ['scss']);
+    gulp.watch(paths.js, gulp.series('scripts'));
+    gulp.watch(paths.scss, gulp.series('scss'));
 });
 
 //remove .DS_Store files
-gulp.task( 'clean:ds', function() {
+gulp.task('clean:ds', function() {
   return gulp.src(root+"/**/.DS_Store", { read: false })
-    .pipe( rm() );
-})
+    .pipe( plugin.clean() );
+});
+
+//remove the destination folder
+gulp.task('clean:dest', function() {
+   return gulp.src(destination, {read: false})
+      .pipe(plugin.clean());
+});
 
 // Default Task
 gulp.task('default', gulp.series('scss', 'scripts', 'watch'));
+gulp.task('clean', gulp.parallel('clean:ds', 'clean:dest'));
